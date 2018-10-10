27bb54d01b27b2f31dbc38ba91a921db5f8f6f2a
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-10-10 14:14:00 +0530

    html

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ const port= 3000;
 
 
 server.listen(port,()=>{
-    console.log('server is listening on port: $(port)');
+    console.log('server is listening on 3000');
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html');
 });
